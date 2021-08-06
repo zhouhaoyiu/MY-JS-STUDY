941a339b24b96bff56734e46d45aeb799b09b74a
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2021-08-06 10:42:55 +0800

    study ssr and bst

---
+++ vuessr.js
@@ -0,0 +1,38 @@
+const Vue = require('vue');
+const server = require('express')();
+
+const template = require('fs').readFileSync('./index.template.html', 'utf-8');
+
+const renderer = require('vue-server-renderer').createRenderer({
+  template,
+});
+
+const context = {
+  title: 'vue ssr',
+  metas: `
+        <meta name="keyword" content="vue,ssr">
+        <meta name="description" content="vue srr demo">
+    `,
+};
+
+server.get('*', (req, res) => {
+  const app = new Vue({
+    data: {
+      url: req.url
+    },
+    template: `<div class='cs'>访问的 URL 是： {{ url }}</div>`,
+    StyleSheet: `.cs{color:red}`,
+  });
+
+  renderer
+    .renderToString(app, context, (err, html) => {
+      console.log(html);
+      if (err) {
+        res.status(500).end('Internal Server Error')
+        return;
+      }
+      res.end(html);
+    });
+})
+
+server.listen(8180);
